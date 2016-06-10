7c9714db7bb5a3fe897285df83571d8c1dd77255
Souma Mondal <mondal.souma@gmail.com>
2016-06-09 23:55:04 -0500

    adds controllable paddle

--- engine.js
+++ engine.js
@@ -18,20 +18,20 @@ var Game = (function() {
     callback();
   };
 
-  var KEY_CODES = { 37: "left", 39: "right", 32: "select" };
+  var KEY_CODES = { 37: "left", 39: "right", 32: "space" };
   this.keys = {};
 
   this.setupInput = function() {
     window.addEventListener("keydown", function(e) {
-      if(KEY_CODES[e.keycode]) {
-        Game.keys[KEY_CODES[e.keycode]] = true;
+      if(KEY_CODES[e.keyCode]) {
+        Game.keys[KEY_CODES[e.keyCode]] = true;
         e.preventDefault();
       }
     }, false);
 
     window.addEventListener("keyup", function(e) {
-      if(KEY_CODES[e.keycode]) {
-        Game.keys[KEY_CODES[e.keycode]] = false;
+      if(KEY_CODES[e.keyCode]) {
+        Game.keys[KEY_CODES[e.keyCode]] = false;
         e.preventDefault();
       }
     }, false);
@@ -47,7 +47,6 @@ var Game = (function() {
         boards[i].draw(Game.ctx);
       }
     }
-
   };
 
   this.setBoard = function(num, board) {
@@ -56,3 +55,100 @@ var Game = (function() {
 
   return this;
 })();
+
+var TitleScreen = function TitleScreen(title, subtitle, clear, callback) {
+  var up = false;
+
+  this.step = function(dt) {
+    if(!Game.keys['space']) {
+      up = true;
+    }
+    if (up && Game.keys['space'] && callback) {
+      callback();
+    }
+  };
+
+  this.draw = function(ctx) {
+    if(clear) {
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0,0,Game.width,Game.height);
+    }
+    ctx.fillStyle = "#FFFFFF";
+    ctx.textAlign = "center";
+
+    ctx.font = "bold 40px bangers";
+    ctx.fillText(title, Game.width/2, Game.height/2);
+
+    ctx.font = "bold 20px bangers";
+    ctx.fillText(subtitle, Game.width/2, Game.height/2 + 40);
+  };
+};
+
+var GameBoard = function() {
+  var board = this;
+
+  this.objects = [];
+  this.cnt = {};
+
+  this.add = function(obj) {
+    obj.board = this;
+    this.objects.push(obj);
+    this.cnt[obj.type] = (this.cnt[obj.type] || 0) + 1;
+    return obj;
+  };
+
+  this.remove = function(obj) {
+    var idx = this.remove.indexOf(obj);
+    if(idx === -1) {
+      this.removed.push(obj);
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  this.resetRemoved = function() {
+    this.removed = [];
+  };
+
+  this.finalizeRemoved = function() {
+    for(var i = 0, len = this.removed.length; i < len; ++i) {
+      var idx = this.objects.indexOf(this.removed[i]);
+      if(idx !== -1) {
+        this.cnt[this.removed[i].type]--;
+        this.objects.splice(idx, 1);
+      }
+    }
+  };
+
+  // Call the same method on all current objects
+  this.iterate = function(funcName) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    for(var i = 0, len = this.objects.length; i < len; ++i) {
+      var obj = this.objects[i];
+      obj[funcName].apply(obj, args);
+    }
+  };
+
+  // find the first object for which func is true
+  // otherwise return false
+  this.detect = function(func) {
+    for(var i = 0, val = null, len = this.objects.length; i < len; ++i) {
+      if( func.call(this.objects[i]) ) {
+        return this.objects[i];
+      }
+    }
+    return false;
+  };
+
+  this.step = function(dt) {
+    this.resetRemoved();
+    this.iterate('step', dt);
+    this.finalizeRemoved();
+  };
+
+  this.draw = function(ctx) {
+    this.iterate('draw', ctx);
+  };
+
+}
--- game.js
+++ game.js
@@ -9,5 +9,52 @@ window.addEventListener("load", function() {
 });
 
 var startGame = function() {
-  
-}
+  Game.setBoard(0, new TitleScreen("blocPONG",
+                                   "Press <space> to RAGE",
+                                   true,
+                                   playGame));
+};
+
+var playGame = function() {
+  var gameBoard = new GameBoard();
+  gameBoard.add( new Paddle(20, 100, 200, true) );
+  Game.setBoard(0, gameBoard);
+};
+
+var Paddle = function(height, width, maxVel, clear) {
+  this.w = width;
+  this.h = height;
+  this.x = Game.width/2 - this.w / 2;
+  // this.y = Game.height - this.h;
+  this.y = Game.height - 10 - this.h;
+  this.maxVel = maxVel;
+  this.clear = clear;
+};
+
+Paddle.prototype.step = function(dt) {
+  if(Game.keys["left"]) {
+    this.vx = -this.maxVel;
+  } else if(Game.keys["right"]) {
+    this.vx = this.maxVel;
+  } else {
+    this.vx = 0;
+  }
+  this.x += this.vx * dt;
+};
+
+Paddle.prototype.draw = function(ctx) {
+
+  if (this.clear) {
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0,0,Game.width,Game.height);
+  }
+  ctx.beginPath();
+  ctx.rect(this.x, this.y, this.w, this.h);
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "#00FF00";
+  ctx.stroke();
+  ctx.fillStyle = "#00FF00";
+  ctx.fill();
+  ctx.closePath();
+  ctx.lineWidth = 1;
+};
